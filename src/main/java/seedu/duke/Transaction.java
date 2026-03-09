a6cb2ee23edd48fbf2663ececab960968cb01cb3
Nishuy52 <jyuc1109sch@gmail.com>
2026-03-09 16:09:07 +0800

    Transaction: Add description field and getter

--- src/main/java/seedu/duke/Transaction.java
+++ src/main/java/seedu/duke/Transaction.java
@@ -7,19 +7,28 @@ public abstract class Transaction {
 
     protected final String category;
     protected final double amount;
+    protected final String description;
 
-    public Transaction(String category, double amount) {
+    public Transaction(String category, double amount, String description) {
         this.category = category;
         this.amount = amount;
+        this.description = description;
     }
 
-    //can be used for list command
+    public Transaction(String category, double amount) {
+        this(category, amount, "");
+    }
+
     public double getAmount() {
         return amount;
     }
 
     public String getCategory() {
         return category;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public abstract String getType();
