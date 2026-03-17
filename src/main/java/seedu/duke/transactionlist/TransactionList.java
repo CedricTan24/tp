560b2e40ba64ab41ae7c0c4cff1f3a1df9117a98
CedricTan24 <e1384331@u.nus.edu>
2026-03-17 14:26:20 +0800

    Add logging to TransactionList

--- src/main/java/seedu/duke/transactionlist/TransactionList.java
+++ src/main/java/seedu/duke/transactionlist/TransactionList.java
@@ -3,13 +3,17 @@ package seedu.duke.transactionlist;
 import seedu.duke.transaction.Transaction;
 
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 //todo: delete
 public class TransactionList {
+
+    private static final Logger logger = Logger.getLogger(TransactionList.class.getName());
     private final ArrayList<Transaction> transactions = new ArrayList<>();
 
     public void add(Transaction t) {
         assert t != null : "Transaction should not be null";
+        logger.info("Adding transaction: " + t);
         transactions.add(t);
     }
 
@@ -27,6 +31,7 @@ public class TransactionList {
     //removes a transaction from list
     public Transaction remove(int i) {
         assert i >= 0 && i < transactions.size() : "Index is out of bounds";
+        logger.info("Removing transaction at index: " + i);
         return transactions.remove(i);
     }
 }
